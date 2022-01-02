59889c017a73545afbdf51ceafdca19111bb71c0
Alexwebcreator <Sanek72Tob@mail.ru>
2022-01-02 18:29:15 +0500

    create form validation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import inputs from './modules/inputs';
 
 timer('07 january 2022');
 menu();
 modal();
+inputs();
---
+++ src/modules/inputs.js
@@ -0,0 +1,49 @@
+const inputs = () => {
+  const square = document.querySelector('.calc-square');
+  const count = document.querySelector('.calc-count');
+  const day = document.querySelector('.calc-day');
+  const names = document.querySelectorAll('.form-name');
+  const topName = document.getElementById('form2-name');
+  const message = document.getElementById('form2-message');
+  const email = document.querySelectorAll('.form-email');
+  const phone = document.querySelectorAll('.form-phone');
+
+
+  const isNumber = (e) => {
+    e.target.value = e.target.value.replace(/\D+/, "");
+  };
+
+  square.addEventListener('input', isNumber);
+  count.addEventListener('input', isNumber);
+  day.addEventListener('input', isNumber);
+
+  const isCyryl = (n) => {
+    n.target.value = n.target.value.replace(/[^а-я\s\-]/gi, "");
+    console.log('hello');
+  };
+
+  topName.addEventListener('input', isCyryl);
+  names.forEach((item) =>
+    item.addEventListener("input", isCyryl)
+  );
+  message.addEventListener('input', isCyryl);
+  
+  const isEmail = (s) => {
+    s.target.value = s.target.value.replace(/[^\w\-\_\.\!\~\*\'@]/gi, "");
+  };
+
+  email.forEach((mail) =>
+    mail.addEventListener("input", isEmail)
+  );
+
+  const isPhone = (m) => {
+    m.target.value = m.target.value.replace(/[^\d\-\(\)]/gi, "");
+  };
+
+  phone.forEach((tel) =>
+    tel.addEventListener("input", isPhone)
+  );
+
+};
+
+export default inputs;
