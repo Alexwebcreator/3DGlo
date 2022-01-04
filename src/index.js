f304340a8afa28a07bdd4c1cd3e1b5458dc0966f
Alexwebcreator <Sanek72Tob@mail.ru>
2022-01-04 17:45:55 +0500

    create slider functions

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import inputs from './modules/inputs';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 timer('07 january 2022');
 menu();
 modal();
 inputs();
 tabs();
+slider();
