f53a766de26fab29f416c0a870b040c9747e3ee2
Alexwebcreator <Sanek72Tob@mail.ru>
2022-01-06 15:51:27 +0500

    create calculator function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal';
 import inputs from './modules/inputs';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import calc from './modules/calc';
 
 timer('07 january 2022');
 menu();
@@ -11,3 +12,4 @@ modal();
 inputs();
 tabs();
 slider();
+calc(100);
